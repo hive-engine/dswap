54d4cef1d3de4e4af9366d138fafadfa04757b6a
horlorlahdeh <horlorlahdeh@gmail.com>
2020-05-27 05:42:11 +0100

    initiate market maker

--- src/components/swapnav/swapnav.ts
+++ src/components/swapnav/swapnav.ts
@@ -1,6 +1,18 @@
-import { customElement, autoinject, bindable } from 'aurelia-framework';
+import { customElement, autoinject, bindable } from "aurelia-framework";
+import { DswapOrderModal } from "../../modals/dswap-order";
+import { DialogService } from "aurelia-dialog";
+
 
 @autoinject()
-@customElement('swapnav')
+@customElement("swapnav")
 export class SwapNav {
+  constructor(private dialogService: DialogService) {}
+  
+  initiateMarketMaker() {
+    this.dialogService
+      .open({ viewModel: DswapOrderModal })
+      .whenClosed((response) => {
+        console.log(response);
+      });
+  }
 }
