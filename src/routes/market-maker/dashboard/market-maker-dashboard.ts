c4cc5747b042657e52bb33d124d8927aae10b777
Ololade Isreal <horlorlahdeh@gmail.com>
2020-07-30 14:35:45 +0100

    Add Market Modal set up

--- src/routes/market-maker/dashboard/market-maker-dashboard.ts
+++ src/routes/market-maker/dashboard/market-maker-dashboard.ts
@@ -1,15 +1,40 @@
 import styles from "./market-maker-dashboard.module.css";
+import { autoinject, bindable } from "aurelia-framework";
+import { AddMarketModal } from "modals/addmarket";
+import { SigninModal } from "modals/signin";
+import { DialogService, DialogCloseResult } from "aurelia-dialog";
+import { DialogController } from "aurelia-dialog";
+import { Store, dispatchify } from "aurelia-store";
 
-
+@autoinject
 export class MarketMakerDashboard {
-           private styles = styles;
-           private page = 1;
+    @bindable router;
+    private styles = styles;
+    private isClicked: false;
 
-         
-           attached() {
-               $(".toggle").click(function (e) {
-                   e.preventDefault(); // The flicker is a codepen thing
-                   $(this).toggleClass("toggle-on");
-               });
-           }
-       }
+    constructor(
+        private dialogService: DialogService,
+
+        private store: Store<IState>
+    ) {}
+
+        attached (){
+            $(".toggle").click(function (e) {
+                e.preventDefault();
+                $(this).toggleClass("toggle-on").toggleClass("toggle-text-off");
+            });
+        }
+    addMarket() {
+        this.dialogService
+            .open({ viewModel: AddMarketModal })
+            .whenClosed((x) => this.walletDialogCloseResponse(x));
+        console.log("market added");
+    }
+    walletDialogCloseResponse(response: DialogCloseResult) {
+        console.log(response);
+
+        // reload data if necessary
+        if (!response.wasCancelled) {
+        }
+    }
+}
