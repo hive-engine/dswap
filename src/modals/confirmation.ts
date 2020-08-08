58e225a5d3188bb3b12f29bb0f239b65a708a98d
Ololade Isreal <horlorlahdeh@gmail.com>
2020-08-08 20:01:08 +0100

    Confirmation modal

---
+++ src/modals/confirmation.ts
@@ -0,0 +1,18 @@
+import { dispatchify, Store } from "aurelia-store";
+import { DialogController } from "aurelia-dialog";
+import { autoinject, TaskQueue, bindable } from "aurelia-framework";
+
+import styles from "./dswap-order.module.css";
+
+@autoinject()
+export class ConfirmationModal {
+    private styles = styles;
+    constructor(
+        private controller: DialogController,
+
+        private store: Store<IState>
+    ) {
+        this.controller.settings.lock = false;
+        this.controller.settings.centerHorizontalOnly = true;
+    }
+}
