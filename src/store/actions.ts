9f3359783de801af2f4e8019aea48d608d0b3ab4
lion-200 <i.duz86@outlook.com>
2020-08-19 22:35:10 +0200

    market maker changes

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -112,7 +112,7 @@ export async function getMarketMakerUser(state: IState): Promise<IState> {
     }
 
     try {
-        let mmUser = await getUser('lion200');
+        let mmUser = await getUser('enginemaker'); // only to see dashboard
         newState.marketMakerUser = mmUser;
     } catch (e) {
         log.error(e);
