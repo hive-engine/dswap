ea6d5ac82e730af14eaafb0eb6d1fd99d2ab6ad5
Ololade Isreal <horlorlahdeh@gmail.com>
2020-08-23 20:00:16 +0100

    toggle ts

--- src/routes/market-maker/dashboard/market-maker-dashboard.ts
+++ src/routes/market-maker/dashboard/market-maker-dashboard.ts
@@ -5,8 +5,8 @@ import { ConfirmationModal } from "modals/confirmation";
 import { DialogService, DialogCloseResult } from "aurelia-dialog";
 import { DialogController } from "aurelia-dialog";
 import { Store, dispatchify } from "aurelia-store";
-import { Router } from 'aurelia-router';
-import { Subscription } from 'rxjs';
+import { Router } from "aurelia-router";
+import { Subscription } from "rxjs";
 import { DisableAccountModal } from "modals/market-maker/disable-account";
 import { getCurrentFirebaseUser } from "store/actions";
 import { EnableAccountModal } from "modals/market-maker/enable-account";
@@ -20,26 +20,34 @@ export class MarketMakerDashboard {
     private user;
     private state: IState;
 
-    constructor(private dialogService: DialogService, private store: Store<IState>, private router: Router) {
-        this.subscription = this.store.state.subscribe(async (state: IState) => {
-            if (state) {
-                this.state = state;
+    constructor(
+        private dialogService: DialogService,
+        private store: Store<IState>,
+        private router: Router
+    ) {
+        this.subscription = this.store.state.subscribe(
+            async (state: IState) => {
+                if (state) {
+                    this.state = state;
 
-                this.user = { ...state.firebaseUser };
-                this.marketMakerUser = { ...state.marketMakerUser };                
+                    this.user = { ...state.firebaseUser };
+                    this.marketMakerUser = { ...state.marketMakerUser };
+                }
             }
-        });
+        );
     }
 
-    async bind() {
-        
-    }
+    async bind() {}
 
     async canActivate() {
         // return to landing page if user doesn't exist in market maker user table
-        if (!this.marketMakerUser || !this.marketMakerUser._id || this.marketMakerUser._id <= 0) {
-            console.log('test');
-            this.router.navigate('market-maker');
+        if (
+            !this.marketMakerUser ||
+            !this.marketMakerUser._id ||
+            this.marketMakerUser._id <= 0
+        ) {
+            console.log("test");
+            this.router.navigate("market-maker");
         }
 
         console.log(this.marketMakerUser);
@@ -71,7 +79,7 @@ export class MarketMakerDashboard {
             this.dialogService
                 .open({ viewModel: DisableAccountModal })
                 .whenClosed((x) => this.walletDialogCloseResponse(x));
-        }        
+        }
 
         console.log("market added");
     }
@@ -81,8 +89,11 @@ export class MarketMakerDashboard {
 
         // reload data if necessary
         if (!response.wasCancelled) {
-            let v = Math.floor((Math.random() * 1000000) + 1);
-            this.router.navigate('market-maker-dashboard?v=' + v, { replace: true, trigger: true });
+            let v = Math.floor(Math.random() * 1000000 + 1);
+            this.router.navigate("market-maker-dashboard?v=" + v, {
+                replace: true,
+                trigger: true,
+            });
         }
     }
 }
