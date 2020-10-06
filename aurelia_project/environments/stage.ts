ddf7bb8b189e3e2b3f838a48af32eaa6e4870bf1
lion-200 <i.duz86@outlook.com>
2020-10-06 12:50:56 +0200

    Update

--- aurelia_project/environments/stage.ts
+++ aurelia_project/environments/stage.ts
@@ -4,7 +4,7 @@ export const environment: Partial<IEnvironment> = {
     ...baseEnvironmentConfiguration,
     debug: false,
     testing: false,    
-    chainId: 'beeab0de00000000000000000000000000000000000000000000000000000000',
+    chainId: 'ssc-mainnet-hive',
     chainId_SE: 'ssc-mainnet1',
     RPC_URL: 'https://api.hive-engine.com/rpc',
     RPC_URL_SE: 'https://api.steem-engine.com/rpc2',
