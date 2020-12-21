aa770e770a57688773b5a9b293248c8d010df60e
lion-200 <i.duz86@outlook.com>
2020-12-21 19:56:33 +0100

    Steem-engine path modifications

--- aurelia_project/environments/stage.ts
+++ aurelia_project/environments/stage.ts
@@ -7,10 +7,10 @@ export const environment: Partial<IEnvironment> = {
     chainId: 'ssc-mainnet-hive',
     chainId_SE: 'ssc-mainnet1',
     RPC_URL: 'https://api.hive-engine.com/rpc',
-    RPC_URL_SE: 'https://api.steem-engine.com/rpc2',
+    RPC_URL_SE: 'https://api.steem-engine.net/rpc2',
     GRAPHQL_API: 'https://graphql-qa.steem.services/',
-    NODE_API_URL: 'https://node-api.steem-engine.com/v1/',
-	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
+    NODE_API_URL: 'https://node-api.steem-engine.net/v1/',
+    ACCOUNTS_API_URL: 'https://api.steem-engine.net/accounts',
     CONVERTER_API_HE: 'https://converter-api.hive-engine.com/api/',
 	nativeToken: 'BEE',
     hivePegAccount: 'honey-swap'
