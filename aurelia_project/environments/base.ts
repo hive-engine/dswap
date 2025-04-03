3f6ebb5953d1e279ae22904f60a17c67d382a71f
lion-200 <lion200.dev@gmail.com>
2025-04-03 11:22:35 +0200

    Move to prod

--- aurelia_project/environments/base.ts
+++ aurelia_project/environments/base.ts
@@ -13,8 +13,8 @@ export const baseEnvironmentConfiguration: Partial<IEnvironment> = {
     EXCHANGE_URL_SE: 'https://steem-engine.net/',
     //DSWAP_API_URL: 'http://localhost:37322/api/',
     //DSWAP_API_URL: 'https://qa.dswap.trade:37322/api/',
-    DSWAP_API_URL: 'https://qa-dswap-api.dswap.trade/api/',    
-    /*DSWAP_API_URL: 'https://dswap-api.dswap.trade/api/',*/
+    //DSWAP_API_URL: 'https://qa-dswap-api.dswap.trade/api/',    
+    DSWAP_API_URL: 'https://dswap-api.dswap.trade/api/',
     /*DSWAP_API_URL: 'https://dswap.azurewebsites.net/api/',*/
     BLOCK_EXPLORER_HE: 'https://he.dtools.dev/',
     BLOCK_EXPLORER_SE: 'https://se.dtools.dev/',
